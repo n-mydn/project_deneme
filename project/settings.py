1d0051a1ba5c9e358ce4b4cf3365a601633639ad
n-mydn <76394581+n-mydn@users.noreply.github.com>
2023-07-12 09:23:35 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -159,8 +159,8 @@ PHONENUMBER_DEFAULT_REGION = 'TR'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
-EMAIL_HOST_USER = 'meydannilufer33@gmail.com'
-EMAIL_HOST_PASSWORD='kugavisilkyqcgfy'
+EMAIL_HOST_USER = 'mail'
+EMAIL_HOST_PASSWORD='password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
